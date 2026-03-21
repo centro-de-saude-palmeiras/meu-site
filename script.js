90f8cc9581f2beefe8eba3f0580ec0a9368b75c7
Joao-Victor-MS <vsjoaomoreira@gmail.com>
2026-03-21 15:36:26 -0300

    atualização do site

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function clicou() {
-  alert("Funcionando! 🚀");
+function toggle(id) {
+  const elemento = document.getElementById(id);
+
+  if (elemento.style.display === "block") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "block";
+  }
 }
